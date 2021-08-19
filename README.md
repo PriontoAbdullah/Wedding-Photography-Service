801e2fbde141692d46ea5833daae3b4da6db6cb5
PriontoAbdullah <priontosoft@gmail.com>
2021-08-19 09:53:42 +0600

    Initial Setup With Tailwind

---
+++ README.md
@@ -0,0 +1,1 @@
+# Wedding-Photography-Service
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>📷 Wedding Photography ❤️</h1>
+    </div>
+  );
+};
+
+export default App;
